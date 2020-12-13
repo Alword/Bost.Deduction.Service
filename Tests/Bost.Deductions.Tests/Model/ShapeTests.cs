e294a26db2c07518a679c6d5a91b609ac05303dc
Артём Слепушко <ASlepushko@croc.ru>
2020-12-13 11:32:26 +0300

    Добавьте файлы проекта.

---
+++ Tests/Bost.Deductions.Tests/Model/ShapeTests.cs
@@ -0,0 +1,19 @@
+﻿using Bost.Deductions.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bost.Deductions.Tests.Model
+{
+	public class ShapeTests
+	{
+		[Fact]
+		public static void LoadShapes()
+		{
+			var types = Shape.LoadShapes();
+		}
+	}
+}
